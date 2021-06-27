8f2e2d5905bc5808aca0d0e82a1e1b6e098f7261
Nitin <nksinghal41@gmail.com>
2021-06-27 10:30:29 +0530

    FC

---
+++ src/main/java/com/dp/creationDesignPattern/BuilderDP.java
@@ -0,0 +1,5 @@
+package com.dp.creationDesignPattern;
+
+public class BuilderDP {
+
+}
---
+++ src/main/java/com/dp/creationDesignPattern/package-info.java
@@ -0,0 +1,17 @@
+package com.dp.creationDesignPattern;
+
+
+//Creational design patterns are composed of two dominant ideas. 
+//One is encapsulating knowledge about which concrete classes the system uses. 
+//Another is hiding how instances of these concrete classes are created and combined.
+
+//Consider applying creational patterns when:
+
+//A system should be independent of how its objects and products are created.
+//A set of related objects is designed to be used together.
+//Hiding the implementations of a class library or product, revealing only their interfaces.
+//Constructing different representation of independent complex objects.
+//A class wants its subclass to implement the object it creates.
+//The class instantiations are specified at run-time.
+//There must be a single instance and client can access this instance at all times.
+//Instance should be extensible without being modified.
